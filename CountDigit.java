0cd224cc80c896720e913dd4b198d38425f6bd69
GOWTHAM292004 <gowthamin209@gmail.com>
2025-12-24 08:40:46 +0530

    Count Digit Program

---
+++ CountDigit.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class CountDigit {
+     public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        int count=0;
+        while(input>0){
+            count++;
+            input=input/10;
+        }
+        System.out.println(count);
+    }
+}
