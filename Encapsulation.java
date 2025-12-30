3c6b2a6a57edf8dac20d4d92ef2fa9f8c1226605
GOWTHAM292004 <gowthamin209@gmail.com>
2025-12-30 16:07:48 +0530

    Encapsulation

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,4 +1,17 @@
+
 class Encapsulation{
+    public static void main(String[] args) {
+        //What is Encapsulation : Encapsulation is binding data and methods together and hiding the internal data by making variables private and accessing them through public methods (getters and setters).
+        //Encapsulation is the process of hiding data and allowing controlled access to it using methods(getter and setter).
+        democlass obj=new democlass();
+        obj.setname("Surya");
+        obj.setage(40);
+        System.out.println(obj.getname());
+        System.out.println(obj.getage());
+    }
+}
+
+class democlass{
     private String name;
     private int age;
     public void setname(String name){
@@ -13,14 +26,5 @@ class Encapsulation{
     public int getage(){
         return age;
     }
-}
-
-class demo{
-    public static void main(String[] args) {
-        Encapsulation obj=new Encapsulation();
-        obj.setname("Surya");
-        obj.setage(40);
-        obj.getname();
-        obj.getage();
-    }
+    
 }
