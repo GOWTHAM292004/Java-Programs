0ba57e71e9d1b978743feef0b85051083af9b136
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-09 17:27:40 +0530

    Happy number program

--- HappyNumber.java
+++ HappyNumber.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 public class HappyNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int temp=a;
-        int length=0;
-        while(temp>0){
-            length++;
-            temp=temp/10;
-        }
-        do{
+        int input=sc.nextInt();//input for happy and unhappy number
+        int temp=input;//for verifing last
+        while(input!=1 && input!=4){//stop when it is happy(1) and unhappy (4) every unhappy number will end in 4
             int sum=0;
-            for(int i=0;i<length;i++){
-                int digit=a%10;
+            while(input>0){//fist take the last number of input square it and use += assignment operator in a variable and remover the number and do the same process unti the number become 0 but the squared number will be stored in the variable convert a=(that variable) . while loop will automatically repeate the process untill the number became 1 and 4
+                int digit=input%10;
                 sum+=digit*digit;
-                a=a/10;
+                input=input/10;
             }
-            a=sum;
-        }while(a!=1);
+            input=sum;
+        }
+        if(input==1){
+            System.out.println(temp+" It is a happy Number");
+        }
+        else{
+            System.out.println(temp+" It is Not a happy Number");
+        }
     }
 }
