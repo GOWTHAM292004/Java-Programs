d1567b85e3bdf8f378a712488f2592c4ad7f5d5d
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-14 13:35:02 +0530

    sum of n natural numbers

---
+++ sum_naturalnumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class sum_naturalnumbers {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int input=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=input;i++){
+            sum+=sum;
+        }
+        System.out.println(sum);
+    }
+}
