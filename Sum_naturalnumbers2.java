7f6c59c8fd0bacea3c10ad240a80c87c3e6a2a4d
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-15 08:44:16 +0530

    Sum of n natural numbers using while loop

---
+++ Sum_naturalnumbers2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Sum_naturalnumbers2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int input=sc.nextInt();
+        int sum=0;
+        int a=1;
+        while(a!=input+1){
+            sum+=a;
+            a++;
+        }
+        System.out.println(sum);
+    }
+}
