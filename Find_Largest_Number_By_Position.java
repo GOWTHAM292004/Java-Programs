e72c6a05b7d0790b9d481d31d7916044ceac2e46
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-22 15:26:00 +0530

    Find Largest Number By Position program

--- Find_Largest_Number_By_Position.java
+++ Find_Largest_Number_By_Position.java
@@ -7,13 +7,21 @@ public class Find_Largest_Number_By_Position {
         int array_size=sc.nextInt();
         int[] arr=new int[array_size];
         for (int i=0;i<array_size;i++){
+            System.out.print("Enter the Element : ");
             arr[i]=sc.nextInt();
         }
-        System.out.print("Enter Which Largest number to find : ");
+        System.out.print("Enter n th Largest index to find : ");
         int Position=sc.nextInt();
-        if(Position<array_size){
+        if(Position<=array_size){
             Arrays.sort(arr);
-            System.out.println(arr[Position]);
+            int[] arr2=new int[arr.length];
+            int a=0;
+            for(int i=arr.length-1;i>=0;i--){
+                arr2[a]=arr[i];
+                a++;
+            }
+            System.out.print("The "+Position+" Largest Number is : ");
+            System.out.println(arr2[Position-1]);
         }
         else{
             System.out.print("Enter the valid Position");
