57ee19e2fd088e7b94742de2fbaaed0c057e13b5
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-26 11:42:13 +0530

    Finding Maxinum and Minimium

--- Finding_MaxandMin.java
+++ Finding_MaxandMin.java
@@ -10,12 +10,29 @@ public class Finding_MaxandMin {
             System.out.print("Enter the Element "+i+": ");
             arr[i]=sc.nextInt();
         }
-        int max=Arrays.stream(arr).max().getAsInt();
-        int min=Arrays.stream(arr).min().getAsInt();
+        //Built-in method with stream in Arrays.
+        // int max=Arrays.stream(arr).max().getAsInt();
+        // int min=Arrays.stream(arr).min().getAsInt();
+        // System.out.print("The maximum Number is : ");
+        // System.out.println(max);
+        // System.out.print("The minimum Number is : ");
+        // System.out.println(min);
+
+        // manual method
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        
         System.out.print("The maximum Number is : ");
         System.out.println(max);
         System.out.print("The minimum Number is : ");
         System.out.println(min);
-
     }
 }
