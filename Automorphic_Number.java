d16929560b306b161cc9360bdf31db3630045679
GOWTHAM292004 <gowthamin209@gmail.com>
2026-01-27 14:14:12 +0530

    Automorphic Number

--- Automorphic_Number.java
+++ Automorphic_Number.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class Automorphic_Number
- {
+public class Automorphic_Number{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Input : ");
         int input=sc.nextInt();
-        int multi=input*input;
-        int Last_Digit=multi%10;
-        if(input==Last_Digit){
+        int square=input*input;
+        int input_size=String.valueOf(input).length();
+        int mod=(int)Math.pow(10, input_size);
+        if(square%mod==input){
             System.out.println("It is a Automorphic Number");
         }
         else{
